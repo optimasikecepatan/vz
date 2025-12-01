48bbd5e100e28fee965bddbb16e240a48342a394
Robby Blupartel <108180556+optimasikecepatan@users.noreply.github.com>
2025-12-01 23:37:32 +0700

    Create trackjshitat.js

---
+++ trackjshitat.js
@@ -0,0 +1,5 @@
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();
